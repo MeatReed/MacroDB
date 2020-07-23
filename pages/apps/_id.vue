5605fe17cdfa32eb8e84ad636415ced14646f8ba
MeatReed <meatreeed@gmail.com>
2020-07-23 22:43:57 +0200

    add interface apps

--- pages/apps/_id.vue
+++ pages/apps/_id.vue
@@ -14,11 +14,61 @@
       <h2>{{ app.name }}</h2>
     </v-row>
     <v-row v-if="!$fetchState.pending" justify="center">
-      <qrcode-vue
-        :value="app.download"
-        class="qrcode text-center"
-        :size="500"
-      />
+      <v-col>
+        <v-card>
+          <v-card-title>
+            Change the QRCode
+          </v-card-title>
+          <v-card-text>
+            <v-select
+              v-model="modelQRCodeLevel"
+              label="QRCode Level"
+              :items="itemsQRCode"
+              dense
+              outlined
+            />
+            <v-slider
+              v-model="QRCodeSize"
+              min="100"
+              max="500"
+              label="QRCode Size"
+            />
+            <qrcode-vue
+              :value="app.download"
+              class="qrcode text-center"
+              :size="QRCodeSize"
+              :level="modelQRCodeLevel"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title class="headline"
+            >{{ app.name }}({{ app.tid }})</v-card-title
+          >
+          <v-card-text
+            >Description: {{ app.description }}<br />Category: {{ app.category
+            }}<br />Author: {{ app.author }}<br />Version: {{ app.version
+            }}<br />Github: {{ app.github }}<br />Download: {{ app.download
+            }}<br />Released at:
+            {{
+              new Date(app.released_at)
+                .toISOString()
+                .slice(0, 19)
+                .replace('T', ' ')
+            }}
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text :href="app.github" color="indigo">
+              Github Repository
+            </v-btn>
+            <v-btn text :href="app.download" color="indigo">
+              Download
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -38,6 +88,9 @@ export default {
   },
   data: () => ({
     app: null,
+    itemsQRCode: ['L', 'M', 'Q', 'H'],
+    modelQRCodeLevel: 'Q',
+    QRCodeSize: 300,
   }),
 }
 </script>
